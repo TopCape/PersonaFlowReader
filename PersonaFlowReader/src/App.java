cc387e40fa463e057db8a62dc439d7ec086daf3f
TopCape <gj4044897@gmail.com>
2024-06-12 01:33:56 +0100

    Update App.java

--- PersonaFlowReader/src/App.java
+++ PersonaFlowReader/src/App.java
@@ -1,10 +1,7 @@
 import java.nio.ByteOrder;
 
 public class App {
-
-    public static final ByteOrder order = ByteOrder.LITTLE_ENDIAN;
-    public static final ByteOrder instructionOrder = ByteOrder.BIG_ENDIAN;
-
+    
     public static void main(String[] args) {
         CmdInterface.run();
 
